a36fc060c6d979d46100d3464bc57c6c9eac058f
matteo <matteo.menand@viacesi.fr>
2022-07-20 15:01:57 +0200

    Filtered null values

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 col_list = ['date','lib_dep','tx_pos','tx_incid','TO','R','hosp','rea','rad','reg_rea','incid_hosp','incid_rea','incid_rad','incid_dchosp','reg_incid_rea','pos']
 
 df = pd.read_csv('table-indicateurs-open-data-dep-2022-07-19-19h00.csv', sep=',', usecols=col_list, encoding='utf-8')
-tab_herault = df.loc[(df["date"]>"2021-12-31") & (df["lib_dep"] == "Hérault"),:]
+tab_herault = df.loc[(df["date"]>"2021-12-31") & (df["date"]<"2022-07-17") & (df["lib_dep"] == "Hérault"),:]
 print('Hérault :', tab_herault)
 
-tab_finistere = df.loc[(df["date"]>"2021-12-31") & (df["lib_dep"]=="Finistère"),:]
+tab_finistere = df.loc[(df["date"]>"2021-12-31") & (df["date"]<"2022-07-17") & (df["lib_dep"]=="Finistère"),:]
 print('Finistère :', tab_finistere)
