f5f6be4b12e6123448e4d2d4ead0fa4a95f185a6
ocegba <82329248+ocegba@users.noreply.github.com>
2022-07-20 15:30:57 +0200

    modif nom

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ tab_finistere = df.loc[(df["date"]>"2021-12-31") & (df["date"]<"2022-07-17") & (
 print('Finistère :', tab_finistere)
 
 plt.scatter(tab_herault.date,tab_herault.tx_incid, c="red", s=2)
-plt.title('HERAULT')
-plt.xlabel('DATE')
+plt.title("Taux d'incidence en % dans l'HERAULT par jour")
+plt.xlabel('Date du jour')
 plt.ylabel("Taux d'incidence en %")
 plt.show()
 
-plt.scatter(tab_finistere.date,tab_finistere.tx_incid, c="black", s=2)
-plt.title('FINISTERE')
-plt.xlabel('DATE')
+plt.scatter(tab_finistere.date,tab_finistere.tx_incid, c="green", s=2)
+plt.title("Taux d'incidence en % dans le FINISTERE par jour")
+plt.xlabel('Date du jour')
 plt.ylabel("Taux d'incidence en %")
 plt.show()
