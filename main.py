6ff4d7048f9187f2bc937b9de8c0c2d1af04a5c8
DUSSARTLuke <lukedussart@hotmail.fr>
2022-07-20 14:40:41 +0200

    suppr colonne inutile

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import pandas as pd
 
+col_list = ['date','lib_dep','tx_pos','tx_incid','TO','R','hosp','rea','rad','reg_rea','incid_hosp','incid_rea','incid_rad','incid_dchosp','reg_incid_rea','pos']
 
+df = pd.read_csv('table-indicateurs-open-data-dep-2022-07-19-19h00.csv', sep=',', usecols=col_list, encoding='utf-8')
 
-col_list = ['dep','date','reg','lib_dep','lib_reg','tx_pos','tx_incid','TO','R','hosp','rea','rad','dchosp','reg_rea','incid_hosp','incid_rea','incid_rad','incid_dchosp','reg_incid_rea','pos','pos_7j','cv_dose1']
-
-df = pd.read_csv('table-indicateurs-open-data-dep-2022-07-19-19h00.csv', sep=',', usecols=col_list, encoding='latin-1')
-
-
-
-print('Nombre d éléves :', (df['dep']))
+print('Nombre d éléves :', (df.describe()))
